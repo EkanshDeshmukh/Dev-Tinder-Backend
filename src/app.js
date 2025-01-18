6e3ef28c3c44ea4d55dcdd8b9e264edabdca3d4f
EkanshDeshmukh <yashudeshmukh25@gmail.com>
2025-01-19 00:07:35 +0530

    Added Basic Express Server

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the basic Express server!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
